fa436234a4c659d2b959fd57d0c0a32b47b6d568
mickeygillis13 <mickeygilllis@sheridan.edu>
2026-09-16 14:19:30 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Artist {
+// TODO
+}; 
+
+class Song {
+    //TODO
+}; 
+
+int main() {
+    // Create instance of Artist object 
+
+    // Create instance of Song object
+
+    // Call song instance display function 
+
+    return 0; 
+}
